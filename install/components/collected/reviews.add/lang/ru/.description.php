fe7f230e8fde4401e0e6c6b1def44bc0b0dcfcc3
ASDAFF <asdaff.asad@yandex.ru>
2020-08-01 16:53:26 +0500

    up

--- install/components/collected/reviews.add/lang/ru/.description.php
+++ install/components/collected/reviews.add/lang/ru/.description.php
@@ -1,5 +1,4 @@
 <?
-
 $MESS ['T_COLLECTED_REVIEWS_NAME'] = "Отзывы COLLECTED";
 $MESS ['T_COLLECTED_REVIEWS_ADD_NAME'] = "Форма добавления отзыва";
 $MESS ['T_COLLECTED_REVIEWS_ADD_DESC'] = "Выводит форму добавления отзыва";
